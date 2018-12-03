41cbe7068c939f5e52c276bc976d2ade711f2c5f
Tron Schell <zoniixyt@gmail.com>
2018-12-03 10:27:57 -0500

    Added approutes

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import requests
 from flask import Flask
 
 
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Index!"
+        usd_rate = float(usd_rate_dontuse.replace(',', ''))
+        satoshiUSD = format(usd_rate*0.00000001, '.8f')
 
+        eur_rate = float(eur_rate_dontuse.replace(',', ''))
+        satoshiEUR = format(eur_rate*0.00000001, '.8f')
+
+
+        returnthis = print("$:", satoshiUSD, "€:", satoshiEUR )
+
+        return returnthis
 url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 response = requests.get(url)
 data = response.text
@@ -21,13 +29,14 @@ eur_rate_dontuse = parsed["bpi"]["EUR"]["rate"]
 def usdConversion():
     usd_rate = float(usd_rate_dontuse.replace(',', ''))
     satoshiUSD = format(usd_rate*0.00000001, '.8f')
+    return satoshiUSD
 
-
-@app.route('eurconversion')
+@app.route('/eurconversion')
 def eurConversion():
     eur_rate = float(eur_rate_dontuse.replace(',', ''))
     satoshiEUR = format(eur_rate*0.00000001, '.8f')
+    return satoshiEUR
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
