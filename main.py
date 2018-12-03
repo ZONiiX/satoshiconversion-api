f4913ee007f7fc3be648dfd09a08a3d88c44f150
Tron Schell <zoniixyt@gmail.com>
2018-12-03 10:13:43 -0500

    Added flask functionality

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import json
 import requests
-import flask
+from flask import Flask
 
 
 app = flask(__name__)
 
 @app.route('/')
-
-
+def index():
+    return "Index!"
 
 url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 response = requests.get(url)
@@ -16,14 +16,18 @@ parsed = json.loads(data)
 usd_rate_dontuse = parsed["bpi"]["USD"]["rate"]
 eur_rate_dontuse = parsed["bpi"]["EUR"]["rate"]
 
+
+@app.route('/usdconversion')
 def usdConversion():
-usd_rate = float(usd_rate_dontuse.replace(',', ''))
-satoshiUSD = format(usd_rate*0.00000001, '.8f')
+    usd_rate = float(usd_rate_dontuse.replace(',', ''))
+    satoshiUSD = format(usd_rate*0.00000001, '.8f')
 
+
+@app.route('eurconversion')
 def eurConversion():
-eur_rate = float(eur_rate_dontuse.replace(',', ''))
-satoshiEUR = format(eur_rate*0.00000001, '.8f')
+    eur_rate = float(eur_rate_dontuse.replace(',', ''))
+    satoshiEUR = format(eur_rate*0.00000001, '.8f')
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run()
